dc01586377f60207cdc347edac69590936548351
Christopher Nemarich <cmn2@pm.me>
2022-09-29 15:37:01 -0400

    Moves utility functions to `R/`
    Renames `common.R` to `utilities.R` and moves it to the newly created `R/` directory

--- R/utilities.R
+++ R/utilities.R
@@ -1,12 +1,11 @@
 #
 # Utility tools
 #
-
 ParsePatientID <- function(x) {
-    return(unlist(strsplit(x, "/"))[2])
+  return(unlist(strsplit(x, "/"))[2])
 }
 
 
 ReplaceNA <- function(df) {
-    return(replace(df, is.na(df), ""))
+  return(replace(df, is.na(df), ""))
 }
