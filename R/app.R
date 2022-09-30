0d01dda89cecb6c60db8de43f82cd3d582848d53
Christopher Nemarich <cmn2@pm.me>
2022-09-30 13:37:38 -0400

    Move `app.R` file
    Moves `app.R` file into `R` directory and also creates function call for `FetchData()` inside server function.

---
+++ R/app.R
@@ -0,0 +1,53 @@
+# UI component
+ui <- navbarPage(
+  "CLOVoc Data Dashboard",
+  tabPanel(
+    "Explore Data",
+    sidebarLayout(
+      sidebarPanel(
+        uiOutput("select"),
+        downloadButton("download", "Download as TSV"),
+        width = 2
+      ),
+      mainPanel(DT::dataTableOutput("records"), width = 10)
+    )
+  )
+)
+
+# server component
+server <- function(input, output) {
+
+  dataset <- FetchData()
+
+  output$select <- renderUI({
+    selectInput(
+      "table",
+      "Select a Table:",
+      c("Patient", "Condition", "Specimen", "DocumentReference"),
+      selected = "Patient"
+    )
+  })
+
+  data_input <- reactive({
+    data <- dataset[[input$table]]
+    return(data)
+  })
+
+  output$download <- downloadHandler(
+    file_name = function() {
+      paste(input$table, "tsv", sep = ".")
+    },
+    content = function(file) {
+      write.table(
+        data_input(), file, sep = "\t", row.names = FALSE
+      )
+    }
+  )
+
+  output$records <- DT::renderDataTable({
+    data_input()
+  }, options = list(pageLength = 25))
+}
+
+# Bind UI and server to an application
+shinyApp(ui = ui, server = server)
